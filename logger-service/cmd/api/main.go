8c6b911ad2db71a5f0ff1b936d9a30c16439777c
rileyarnie <arnold.ril3y@gmail.com>
2025-02-17 11:41:42 +0300

    create logger service

---
+++ logger-service/cmd/api/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"log-service/data"
+	"net/http"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	PORT     = "80"
+	rpcPort  = "5001"
+	mongoURL = "mongodb://localhost:27017"
+	gRpcPort = "50001"
+)
+
+var client *mongo.Client
+
+type Config struct {
+	Models data.Models
+}
+
+func main() {
+
+	//connect to mongo
+	mongoClient, err := connectToMongo()
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	client = mongoClient
+
+	//create context to disconnect
+
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	//close connection
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	app := Config{
+		Models: data.New(client),
+	}
+
+	//start webserver
+	// go app.serve()
+	log.Println("Starting the logging service on port", PORT)
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", PORT),
+		Handler: app.routes(),
+	}
+
+	err = srv.ListenAndServe()
+
+	if err != nil {
+		log.Panic()
+	}
+
+}
+
+// func (app *Config) serve() {
+
+// 	srv := &http.Server{
+// 		Addr:    fmt.Sprintf(":%s", PORT),
+// 		Handler: app.routes(),
+// 	}
+
+// 	err := srv.ListenAndServe()
+
+// 	if err != nil {
+// 		log.Panic()
+// 	}
+// }
+
+func connectToMongo() (*mongo.Client, error) {
+	//create connection options
+
+	clientOptions := options.Client().ApplyURI(mongoURL)
+	clientOptions.SetAuth(options.Credential{
+		Username: "admin",
+		Password: "password",
+	})
+
+	//connect to BD
+	c, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Println("Error connecting to database from logger service")
+
+		return nil, err
+	}
+
+	log.Println("Connected to mongo")
+
+	return c, nil
+
+}

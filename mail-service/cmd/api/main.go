06b78530f7f452efdf5fab185c40ec0a6daef604
rileyarnie <arnold.ril3y@gmail.com>
2025-02-18 18:39:07 +0300

    add logic to send email

---
+++ mail-service/cmd/api/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Config struct{}
+
+const PORT = "80"
+
+func main() {
+	app := Config{}
+
+	log.Println("Starting mail service on port", PORT)
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", PORT),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+
+	if err != nil {
+		log.Panic(err)
+	}
+}
